b8ef927b5e9404a18773f38e06e46efb2e1d57a4
Stanislaw Masalskiy <admiral.happiness@gmail.com>
2014-12-14 17:26:22 +0200

    Metrics

--- app/views/hello.php
+++ app/views/hello.php
@@ -172,5 +172,31 @@ Minify::javascript([
 <!--    <script src="/js/controllers/page/AddCatController.js"></script>-->
     <!--End Controllers-->
 
+<script type="text/javascript">
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter27469905 = new Ya.Metrika({id:27469905,
+                    webvisor:true,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true});
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="//mc.yandex.ru/watch/27469905" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+
 </body>
 </html>
